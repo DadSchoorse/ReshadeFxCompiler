fe9f7b2bff137fa43e561b6ae3bb01ebd398be25
Georg Lehmann <dadschoorse@gmail.com>
2020-01-03 22:21:04 +0100

    include debug info

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
     }
 
     std::unique_ptr<reshadefx::codegen> codegen(
-        reshadefx::create_codegen_spirv(true /* vulkan semantics */, false /* debug info */, true /* uniforms to spec constants */, true /*flip vertex shader*/));
+        reshadefx::create_codegen_spirv(true /* vulkan semantics */, true /* debug info */, true /* uniforms to spec constants */, true /*flip vertex shader*/));
     if (!parser.parse(std::move(preprocessor.output()), codegen.get()))
     {
         return 1;
